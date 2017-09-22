60686cf640ba5e10ef5a3270b616da103d809f3f
caipirginka <github@oicom.com>
2017-09-22 16:32:38 +0200

    Uses separated "app_ios" and "app_android" sections.

--- scripts/create_android_strings.js
+++ scripts/create_android_strings.js
@@ -161,7 +161,7 @@ function processResult(context, lang, langJson, stringXmlJson) {
         }
     });
 
-    var langJsonToProcess = _.assignIn(langJson.config_android, langJson.app);
+    var langJsonToProcess = _.assignIn(langJson.config_android, langJson.app_android);
 
     //now iterate through langJsonToProcess
     _.forEach(langJsonToProcess, function(val, key){
